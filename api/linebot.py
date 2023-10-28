576dc89e47b9749f04ccc95b082386919950abdc
timomt13 <128204105+timomt13@users.noreply.github.com>
2023-10-28 14:59:14 +0800

    Update linebot.py

--- api/linebot.py
+++ api/linebot.py
@@ -57,7 +57,7 @@ def handle_message(event):
         buttons_template = TemplateSendMessage(
             alt_text = 'buttons template',
             template = ButtonsTemplate(
-                thumbnail_image_url='https://img.gmw.cn/imgsports/attachement/jpg/site2/20140616/9cb70d6c6f981508786a1e.jpg',
+                thumbnail_image_url='http://image.uczzd.cn/7744985882967685776.jpg?id=0',
                 title = 'He is the G.O.A.T Messi',
                 text = 'He is the king of football',
                 actions = [
